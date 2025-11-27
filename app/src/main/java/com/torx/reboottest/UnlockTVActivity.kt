f3c15291d1a15a36743dac5e1e3be8daa5fad29b
rehmanjameel <abdulrehmancs17@gmail.com>
2025-11-27 12:41:24 +0500

    make changes

--- app/src/main/java/com/torx/reboottest/UnlockTVActivity.kt
+++ app/src/main/java/com/torx/reboottest/UnlockTVActivity.kt
@@ -1,10 +1,15 @@
 package com.torx.reboottest
 
+import android.Manifest
+import android.app.KeyguardManager
 import android.os.Build
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import androidx.annotation.RequiresPermission
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -13,50 +18,59 @@ import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.torx.reboottest.services.TOTPVerifier
 
+
 class UnlockTVActivity : AppCompatActivity() {
 
     private lateinit var unlockET: TextInputEditText
     private lateinit var unlockTIL: TextInputLayout
     private lateinit var unlockButton: MaterialButton
-    private val secretKey = "JBSWY3DPEHPK3PXP"   // same secret saved earlier
+    private val secretKey = "JBSWY3DPEHPK3PXP"
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_unlock_tvactivity)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         unlockET = findViewById(R.id.unlockTIET)
         unlockTIL = findViewById(R.id.unlockTIL)
         unlockButton = findViewById(R.id.unlockButton)
+
         unlockButton.setOnClickListener {
             val inputCode = unlockET.text.toString()
 
             if (inputCode.length != 6) {
-                unlockTIL.error = "Enter 6‑digit code"
+                unlockTIL.error = "Enter 6-digit code"
                 return@setOnClickListener
             }
 
             val success = TOTPVerifier.verifyCode(secretKey, inputCode)
 
             if (success) {
-                unlockTIL.error = ""
+                unlockTIL.error = null
                 unlockTV()
             } else {
-                unlockTIL.error = "Invalid code! Try again."
+                unlockTIL.error = "Invalid code!"
             }
+        }
+
+        // LOCK DEVICE INTO YOUR APP
+        try {
+            startLockTask()   // home/back locked
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
     private fun unlockTV() {
-        Toast.makeText(this, "Unlocked!", Toast.LENGTH_SHORT).show()
+        try {
+            stopLockTask()  // unlock device
+        } catch (e: Exception) {}
 
-        // Example: finish your kiosk mode
+        Toast.makeText(this, "Unlocked!", Toast.LENGTH_SHORT).show()
         finishAffinity()
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        return true // consume all keys
     }
 }
